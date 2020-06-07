bf44a30f2bfcaa0491d571725b1a4a81bf5df604
IrisReijnen <55184866+IrisReijnen@users.noreply.github.com>
2020-06-07 19:49:03 +0200

    Add files via upload
    extra filter verandert de Database.py, Resultaten.html en app.py

--- Database.py
+++ Database.py
@@ -2,6 +2,11 @@ import mysql.connector
 
 
 def database():
+    """Haalt de gegevens in de database op.
+
+    :return: Een lijst met proteine naam, een lijst met de lineage,
+    een lijst met de functie
+    """
     conn = mysql.connector.connect(
         host="opusflights.com",
         user="course4",
@@ -23,7 +28,21 @@ def database():
     return list_protein_name, list_lineage, list_description
 
 
-def database_filter(filter, checkbox_p, checkbox_s, checkbox_f):
+def database_filter(filter, checkbox_p, checkbox_s, checkbox_f, checkbox_o):
+    """Filtert in de database volgens
+
+    :param filter: Een string waarop gefilterd wordt
+    :param checkbox_p: Een boolean of er wel of niet gefilterd wordt in
+    de proteine naam
+    :param checkbox_s: Een boolean of er wel of niet gefilterd wordt in
+    de lineage
+    :param checkbox_f: Een boolean of er wel of niet gefilterd wordt in
+    de functie
+    :param checkbox_o: Een boolean of er wel of niet gefilterd wordt in
+    het organisme
+    :return: Een lijst met proteine naam, een lijst met de lineage,
+    een lijst met de functie
+    """
     conn = mysql.connector.connect(
         host="opusflights.com",
         user="course4",
@@ -35,53 +54,98 @@ def database_filter(filter, checkbox_p, checkbox_s, checkbox_f):
     list_description = []
     if checkbox_p:
         if checkbox_s:
-            if checkbox_f:  # p:True s:True f:True
-                mysql_insert_query = """select protein_name, lineage, description 
-                from protein_seq join taxonomy t on protein_seq.taxonomy_tax_id = t.tax_id 
-                where protein_name like %s or lineage like %s or description like %s
-                limit 10;"""
-                query_input = (
-                "%" + filter + "%", "%" + filter + "%", "%" + filter + "%")
-            else:  # p:True s:True f:False
-                mysql_insert_query = """select protein_name, lineage, description 
-                from protein_seq join taxonomy t on protein_seq.taxonomy_tax_id = t.tax_id
-                where protein_name like %s or lineage like %s
-                limit 10;"""
-                query_input = ("%" + filter + "%", "%" + filter + "%")
+            if checkbox_f:
+                if checkbox_o:# p:True s:True f:True o:True
+                    mysql_insert_query = """select protein_name, lineage, description 
+                    from protein_seq join taxonomy t on protein_seq.taxonomy_tax_id = t.tax_id 
+                    where protein_name like %s or lineage like %s or description like %s or organism like %s;"""
+                    query_input = ("%" + filter + "%", "%" + filter + "%", "%"
+                                   + filter + "%", "%" + filter + "%")
+                else: #p:True s:True f:True o:False
+                    mysql_insert_query = """select protein_name, lineage, description 
+                                        from protein_seq join taxonomy t on protein_seq.taxonomy_tax_id = t.tax_id 
+                                        where protein_name like %s or lineage like %s or description like %s;"""
+                    query_input = ("%" + filter + "%", "%" + filter + "%", "%"
+                                   + filter + "%")
+            else:
+                if checkbox_o: # p:True s:True f:False o:True
+                    mysql_insert_query = """select protein_name, lineage, description 
+                    from protein_seq join taxonomy t on protein_seq.taxonomy_tax_id = t.tax_id
+                    where protein_name like %s or lineage like %s or organism like %s;"""
+                    query_input = ("%" + filter + "%", "%" + filter + "%", "%" + filter + "%")
+                else: #p:True s:True f:False o:False
+                    mysql_insert_query = """select protein_name, lineage, description 
+                                        from protein_seq join taxonomy t on protein_seq.taxonomy_tax_id = t.tax_id
+                                        where protein_name like %s or lineage like %s;"""
+                    query_input = ("%" + filter + "%", "%" + filter + "%")
         else:
-            if checkbox_f:  # p:True s:False f:True
-                mysql_insert_query = """select protein_name, lineage, description 
-                from protein_seq join taxonomy t on protein_seq.taxonomy_tax_id = t.tax_id
-                where protein_name like %s or description like %s
-                limit 10;"""
-                query_input = ("%" + filter + "%", "%" + filter + "%")
-            else:  # p:True s:False f:False
-                mysql_insert_query = """select protein_name, lineage, description 
-                from protein_seq join taxonomy t on protein_seq.taxonomy_tax_id = t.tax_id 
-                where protein_name like %s
-                limit 10;"""
-                query_input = ("%" + filter + "%",)
+            if checkbox_f:
+                if checkbox_o: # p:True s:False f:True o:True
+                    mysql_insert_query = """select protein_name, lineage, description 
+                                        from protein_seq join taxonomy t on protein_seq.taxonomy_tax_id = t.tax_id
+                                        where protein_name like %s or description like %s or organism like %s;"""
+                    query_input = ("%" + filter + "%", "%" + filter + "%", "%" + filter + "%")
+                else:# p:True s:False f:True o:False
+                    mysql_insert_query = """select protein_name, lineage, description 
+                    from protein_seq join taxonomy t on protein_seq.taxonomy_tax_id = t.tax_id
+                    where protein_name like %s or description like %s;"""
+                    query_input = ("%" + filter + "%", "%" + filter + "%")
+            else:
+                if checkbox_o: # p:True s:False f:False o:True
+                    mysql_insert_query = """select protein_name, lineage, description 
+                                        from protein_seq join taxonomy t on protein_seq.taxonomy_tax_id = t.tax_id 
+                                        where protein_name like %s or organism like %s;"""
+                    query_input = ("%" + filter + "%", "%" + filter + "%")
+                else: # p:True s:False f:False o:False
+                    mysql_insert_query = """select protein_name, lineage, description 
+                    from protein_seq join taxonomy t on protein_seq.taxonomy_tax_id = t.tax_id 
+                    where protein_name like %s;"""
+                    query_input = ("%" + filter + "%",)
     else:
         if checkbox_s:
-            if checkbox_f:  # p:False s:True f:True
-                mysql_insert_query = """select protein_name, lineage, description 
-                from protein_seq join taxonomy t on protein_seq.taxonomy_tax_id = t.tax_id
-                where lineage like %s or description like %s
-                limit 10;"""
-                query_input = ("%" + filter + "%", "%" + filter + "%")
-            else:  # p:False s:True f:False
-                mysql_insert_query = """select protein_name, lineage, description 
-                from protein_seq join taxonomy t on protein_seq.taxonomy_tax_id = t.tax_id
-                where lineage like %s
-                limit 10;"""
-                query_input = ("%" + filter + "%",)
+            if checkbox_f:
+                if checkbox_o: # p:False s:True f:True o:True
+                    mysql_insert_query = """select protein_name, lineage, description 
+                                        from protein_seq join taxonomy t on protein_seq.taxonomy_tax_id = t.tax_id
+                                        where lineage like %s or description like %s or organism like %s;"""
+                    query_input = ("%" + filter + "%", "%" + filter + "%", "%" + filter + "%")
+                else: # p:False s:True f:True o:False
+                    mysql_insert_query = """select protein_name, lineage, description 
+                    from protein_seq join taxonomy t on protein_seq.taxonomy_tax_id = t.tax_id
+                    where lineage like %s or description like %s;"""
+                    query_input = ("%" + filter + "%", "%" + filter + "%")
+            else:  
+                if checkbox_o: # p:False s:True f:False o:True
+                    mysql_insert_query = """select protein_name, lineage, description 
+                                        from protein_seq join taxonomy t on protein_seq.taxonomy_tax_id = t.tax_id
+                                        where lineage like %s or organism like %s;"""
+                    query_input = ("%" + filter + "%", "%" + filter + "%")
+                else: # p:False s:True f:False o:False
+                    mysql_insert_query = """select protein_name, lineage, description 
+                    from protein_seq join taxonomy t on protein_seq.taxonomy_tax_id = t.tax_id
+                    where lineage like %s;"""
+                    query_input = ("%" + filter + "%",)
         else:
-            if checkbox_f:  # p:False s:False f:True
-                mysql_insert_query = """select protein_name, lineage, description 
-                from protein_seq join taxonomy t on protein_seq.taxonomy_tax_id = t.tax_id
-                where description like %s
-                limit 10;"""
+            if checkbox_f:  
+                if checkbox_o: # p:False s:False f:True o:True
+                    mysql_insert_query = """select protein_name, lineage, description 
+                                        from protein_seq join taxonomy t on protein_seq.taxonomy_tax_id = t.tax_id
+                                        where description like %s or organism like %s;"""
+                    query_input = ("%" + filter + "%", "%" + filter + "%")
+                else: # p:False s:False f:True o:False
+                    mysql_insert_query = """select protein_name, lineage, description 
+                    from protein_seq join taxonomy t on protein_seq.taxonomy_tax_id = t.tax_id
+                    where description like %s;"""
                 query_input = ("%" + filter + "%",)
+            else:
+                if checkbox_o: # p:False s:False f:False o:True
+                    mysql_insert_query = """select protein_name, lineage, description 
+                                        from protein_seq join taxonomy t on protein_seq.taxonomy_tax_id = t.tax_id
+                                        where organism like %s;"""
+                    query_input = ("%" + filter + "%",)
+                else: # All False p:False s:False f:False o:False
+                    mysql_insert_query = """select protein_name, lineage, description 
+                                        from protein_seq join taxonomy t on protein_seq.taxonomy_tax_id = t.tax_id"""
     cursor.execute(mysql_insert_query, query_input)
     for i in cursor:
         list_protein_name.append(i[0])
@@ -90,7 +154,3 @@ def database_filter(filter, checkbox_p, checkbox_s, checkbox_f):
     cursor.close()
     conn.close()
     return list_protein_name, list_lineage, list_description
-
-
-if __name__ == "__main__":
-    database()
--- app.py
+++ app.py
@@ -6,21 +6,27 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def display():
+    """Laat de Start pagina zien."""
     return render_template("StartPagina.html")
 
 
 @app.route("/Resultaten.html", methods=['POST', 'GET'])
 def display_resultaten():
+    """Geeft filter informatie door aan de database filter functie en laat de
+    gekragen gegevens zien op de Resultaten pagina."""
     if request.method == 'POST':
         filteren = request.form['filter']
-        checkbox_p = ('protein_name' in request.form.keys())
-        checkbox_s = ('stam' in request.form.keys())
+        checkbox_p = ('Protein_name' in request.form.keys())
+        checkbox_s = ('Stam' in request.form.keys())
         checkbox_f = ('Function' in request.form.keys())
+        checkbox_o = ('Organisme' in request.form.keys())
         if filteren == "":
-            list_protein_name, list_lineage, list_description = Database.database()
+            list_protein_name, list_lineage, list_description = \
+                Database.database()
         else:
-            list_protein_name, list_lineage, list_description = Database.database_filter(filteren, checkbox_p, checkbox_s,
-                                            checkbox_f)
+            list_protein_name, list_lineage, list_description = \
+                Database.database_filter(filteren, checkbox_p, checkbox_s,
+                                         checkbox_f, checkbox_o)
         length = list(range(len(list_protein_name)))
         return render_template("Resultaten.html", filter=filteren,
                                length=length,
